6cf4518824896bc44e7662f20c47809746f128d7
EngineeringArmy1 <155462254+EngineeringArmy1@users.noreply.github.com>
2024-02-26 14:39:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('OptionContainer').addEventListener('mouseenter', function() {
+    document.getElementById('OptionContainer').style.backgroundColor = '#2980b9';
+  });
+  
+  document.getElementById('optionButton').addEventListener('mouseleave', function() {
+    document.getElementById('OptionContainer').style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+  });
+  
