097497cc0626cddc3dba2944f0d5224485d2f7d6
EngineeringArmy1 <155462254+EngineeringArmy1@users.noreply.github.com>
2024-03-12 09:40:01 +0700

    Update script.js
    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,103 @@
-document.getElementById('OptionContainer').addEventListener('mouseenter', function() {
-    document.getElementById('OptionContainer').style.backgroundColor = '#2980b9';
-  });
-  
-  document.getElementById('optionButton').addEventListener('mouseleave', function() {
-    document.getElementById('OptionContainer').style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-  });
-  
+document.getElementById('login1').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer1').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login1').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer1').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login2').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer2').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login2').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer2').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login3').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer3').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login3').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer3').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login4').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer4').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login4').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer4').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login5').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer5').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login5').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer5').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login6').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer6').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login6').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer6').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login7').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer7').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login7').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer7').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login8').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer8').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login8').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer8').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login9').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer9').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login9').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer9').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login10').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer10').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login10').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer10').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login11').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer11').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login11').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer11').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login12').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer12').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login12').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer12').style.backgroundColor = '#f1f50a';
+});
+
+document.getElementById('login13').addEventListener('mouseenter', function() {
+  document.getElementById('loginContainer13').style.backgroundColor = '#edf2f3';
+});
+
+document.getElementById('login13').addEventListener('mouseleave', function() {
+  document.getElementById('loginContainer13').style.backgroundColor = '#f1f50a';
+});
